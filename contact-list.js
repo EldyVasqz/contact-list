e6c3236360ffe5dc34727044655e352d78cc1830
EldyVasqz <eldyarely17@gmail.com>
2023-07-18 10:24:44 -0600

    Se modificaron las funciones de crear y elimiar contactos y se almaceno mas informacion

--- contact-list.js
+++ contact-list.js
@@ -1,30 +1,48 @@
-let contactos=["Jose Vasquez","Leyla Vasquez","Nahum Reyes"];
+let contactos=[{
+    id:1,
+    nombres: "Jose",
+    apellidos: "Vasquez",
+    telefono:"+504 96921696",
+    ubicacion:{
+        ciudad:"Siguatepeque",
+        direccion:"Barrio Fatima"
+}
+}]
+
 imprimirContactos(contactos);
-console.log("");
-
-const arrayContactosAñadidos=nuevoContacto(contactos, "Karla Castillo"); 
-console.log("");
-eliminarContacto(arrayContactosAñadidos);
-
-function nuevoContacto(listaContactos,nombreContacto) {
-    console.log("");
-    console.log("Agrega un contacto");
-    listaContactos.push(nombreContacto);
-    imprimirContactos(listaContactos);
-    return listaContactos;
+function imprimirContactos(arrayContactos) {
+    for (let i=0;i<=arrayContactos.length-1;i++)
+    {
+        console.log(arrayContactos[i].id + ". " + arrayContactos[i].nombres + " " + arrayContactos[i].apellidos);
+        console.log("Movil: " + arrayContactos[i].telefono);
+        console.log("Ubicacion: " + arrayContactos[i].ubicacion.ciudad + ", " + arrayContactos[i].ubicacion.direccion );
+    }
+        console.log(" ");
 }
 
-function eliminarContacto(arraysContactos) {
+function agregarContacto(id, nombres, apellidos, telefono, ciudad, direccion ){
+    let nuevo={//funcion
+        id:id,
+        nombres:nombres,
+        apellidos:apellidos,
+        telefono:telefono,
+        ubicacion:{
+            ciudad:ciudad,
+            direccion:direccion
+        }
+    }
+    contactos.push(nuevo);
+}
+
+agregarContacto(2, "Leyla", "Vasquez", "+504 94757879", "Siguatepeque", "Barrio Parnaso" );
+agregarContacto(3, "Nahum", "Reyes", "+504 98457485", "Siguatepeque", "Barrio Parnaso" );
+agregarContacto(4, "Karla", "Castillo", "+504 97485747", "Zacapa", "Barrio Arriba" );
+
+imprimirContactos(contactos);
+
+function eliminarContacto(contactos) {
     console.log("");
     console.log("Elimina un contacto");
-    arraysContactos.pop();
-    imprimirContactos(arraysContactos);
-}
-
-function imprimirContactos(contactos) {
-    let contacto;
-    for (contacto of contactos)
-    {
-        console.log(contacto)
-    }
+    contactos.pop();
+    imprimirContactos(contactos);
 }
